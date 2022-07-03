705cb7796c1e06d05b8551604d85ed9c58591a9c
kovalhood <90193930+kovalhood@users.noreply.github.com>
2022-07-03 12:18:38 +0000

    Deploying to gh-pages from @ kovalhood/randomizer@55318fb0c85f1246d725a135ac17be51bbf46e9d 🚀

--- index.856598f9.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".button_randomize"),o=document.querySelector(".number_rundomize"),t=document.querySelector("#random-from"),n=document.querySelector("#random-to"),r=[];e.addEventListener("click",(function(){o.textContent=(e=t.value,u=n.value,e=Math.ceil(e),u=Math.floor(u),Math.floor(Math.random()*(u-e+1))+e),r.push(o.textContent),console.log(r);var e,u}))}();
-//# sourceMappingURL=index.856598f9.js.map
