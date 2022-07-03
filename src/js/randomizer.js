57b44021a7c83ca398b5101747e6d84e80eabd96
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-03 17:36:09 +0300

    Hiding number functionality

--- src/js/randomizer.js
+++ src/js/randomizer.js
@@ -5,13 +5,14 @@ const inputFrom = document.querySelector('#random-from');
 const inputTo = document.querySelector('#random-to');
 const inputAmount = document.querySelector('#random-amount');
 
-const textBeforeNumber = document.querySelector('.randomize__number--text');
+const numberWrapper = document.querySelector('.randomize__number-wrapper');
+const textBeforeNumber = document.querySelector('.randomize__number-text');
 const randomNumber = document.querySelector('.randomize__number');
 
 let generatedNumbers = [];
 
 randomizeButton.addEventListener('click', handleRandomizeButton);
-clearButton.addEventListener('click', handleClearButton);
+clearButton.addEventListener('click', handleClear);
 
 function getRandomNumber(min, max) {
     min = Math.ceil(min);
@@ -24,23 +25,25 @@ function handleRandomizeButton() {
     generatedNumbers = [];
 }
 
-function handleClearButton() {
+function handleClear() {
     randomNumber.textContent = '0';
+    numberWrapper.classList.add('hidden');
 }
 
 function numbersGeneration(from, to, amount) {
     if (to - amount < 0) {
-        randomNumber.textContent = '0';
-        textBeforeNumberHandler();
+        handleClear();
         return console.log('error');
     }
+
+    numberWrapper.classList.remove('hidden');
 
     for (let i = 0; i < amount; i++) {
         let includesNumber = true;
 
-        // while operator stops execution when includesNumber = false, 
-        // and then number pushes into array generatedNumbers
         while (includesNumber) {
+            // while operator stops execution when includesNumber = false, 
+            // and then number pushes into array generatedNumbers
             number = getRandomNumber(from, to);
             includesNumber = generatedNumbers.includes(number);
         }
@@ -55,7 +58,7 @@ function textBeforeNumberHandler() {
     if (generatedNumbers.length > 1) {
         return textBeforeNumber.textContent = 'Your numbers: ';
     }
-
+    
     textBeforeNumber.textContent = 'Your number: ';
 }
 
