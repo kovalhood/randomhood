1f804cf14509a5de6eacda347100f535d426654d
kovalhood <90193930+kovalhood@users.noreply.github.com>
2022-07-03 12:42:18 +0000

    Deploying to gh-pages from @ kovalhood/randomizer@4a1a51b7ed23c9a481f856a43bb74f0292f6098a 🚀

--- index.ac19c066.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".button_randomize"),t=document.querySelector(".button_clear"),o=document.querySelector(".number_rundomize"),n=document.querySelector("#random-from"),r=document.querySelector("#random-to"),c=[];e.addEventListener("click",(function(){o.textContent=(e=n.value,t=r.value,e=Math.ceil(e),t=Math.floor(t),Math.floor(Math.random()*(t-e+1))+e),c.push(o.textContent),console.log(c);var e,t})),t.addEventListener("click",(function(){o.textContent="0",c=[],console.log(c)}))}();
-//# sourceMappingURL=index.ac19c066.js.map
