8ecd939d6aa9de9c59a3cfedfab2b4a8281b8dfc
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-03 16:50:37 +0300

    Added Extra Functions
    Added Amount Of Unique Numbers Generation Functionality

--- src/js/randomizer.js
+++ src/js/randomizer.js
@@ -1,11 +1,14 @@
 const randomizeButton = document.querySelector('.randomize_button--generate');
 const clearButton = document.querySelector('.randomize_button--clear');
-const randomNumber = document.querySelector('.randomize_number');
-const randomizeHistory = document.querySelector('.randomize_history');
+
 const inputFrom = document.querySelector('#random-from');
 const inputTo = document.querySelector('#random-to');
+const inputAmount = document.querySelector('#random-amount');
 
-let history = [];
+const randomNumber = document.querySelector('.randomize_number');
+const randomizeHistory = document.querySelector('.randomize_history');
+
+let generatedNumbers = [];
 
 randomizeButton.addEventListener('click', handleRandomizeButton);
 clearButton.addEventListener('click', handleClearButton);
@@ -17,15 +20,32 @@ function getRandomNumber(min, max) {
 }
 
 function handleRandomizeButton() {
-    randomNumber.textContent = getRandomNumber(inputFrom.value, inputTo.value);
-    history.push(randomNumber.textContent);
-    randomizeHistory.textContent = history.join(', ');
+    numbersGeneration(inputFrom.value, inputTo.value, inputAmount.value);
+    generatedNumbers = [];
 }
 
 function handleClearButton() {
     randomNumber.textContent = '0';
-    history = [];
-    randomizeHistory.textContent = '';
 }
 
+function numbersGeneration(from, to, amount) {
+    if (to - amount < 0) {
+        randomNumber.textContent = '0';
+        return console.log('error');
+    }
+
+    for (let i = 0; i < amount; i++) {
+        let includesNumber = true;
+
+        // while operator stops execution when includesNumber = false, 
+        // and then number pushes into array generatedNumbers
+        while (includesNumber) {
+            number = getRandomNumber(from, to);
+            includesNumber = generatedNumbers.includes(number);
+        }
+
+        generatedNumbers.push(number);
+        randomNumber.textContent = generatedNumbers.join(', ');
+    }
+}
 
