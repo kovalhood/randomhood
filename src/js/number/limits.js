1a2e01cd8141d86a5f5c5b34a6057ffc92ec1bc5
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-09 20:28:48 +0300

    Added limits

---
+++ src/js/number/limits.js
@@ -0,0 +1,8 @@
+const fromMin = 0;
+const fromMax = 9999;
+const toMin = 1;
+const toMax = 10000;
+const amountMin = 2;
+const amountMax = 100;
+
+export { fromMin, fromMax, toMin, toMax, amountMin, amountMax };
--- src/js/number/number-inputs.js
+++ src/js/number/number-inputs.js
@@ -1,6 +1,5 @@
 import { inputFrom, inputTo, inputAmount, sortType, duplicatesCheckbox, quantity } from './refs';
-
-
+import { fromMin, fromMax, toMin, toMax, amountMin, amountMax } from './limits';
 
 quantity.fromRemove.addEventListener('click', handleFromRemoveClick);
 quantity.fromAdd.addEventListener('click', handleFromAddClick);
@@ -13,13 +12,13 @@ quantity.amountAdd.addEventListener('click', handleAmountAddClick);
 function handleFromRemoveClick() {
     inputFrom.value = Number(inputFrom.value) - 1;
 
-    if (inputFrom.value == 0) {
+    if (inputFrom.value == fromMin) {
         quantity.fromRemove.classList.remove('randomize__quantity');
         quantity.fromRemove.classList.add('randomize__quantity--disabled');
         return quantity.fromRemove.disabled = true;
     }
 
-    if (inputFrom.value <= 9998) {
+    if (inputFrom.value <= fromMax - 1) {
         quantity.fromAdd.classList.add('randomize__quantity');
         quantity.fromAdd.classList.remove('randomize__quantity--disabled');
         quantity.fromAdd.disabled = false;
@@ -29,13 +28,13 @@ function handleFromRemoveClick() {
 function handleFromAddClick() {
     inputFrom.value = Number(inputFrom.value) + 1;
 
-    if (inputFrom.value >= 1) {
+    if (inputFrom.value >= fromMin + 1) {
         quantity.fromRemove.classList.add('randomize__quantity');
         quantity.fromRemove.classList.remove('randomize__quantity--disabled');
         quantity.fromRemove.disabled = false;
     }
 
-    if (inputFrom.value == 9999) {
+    if (inputFrom.value == fromMax) {
         quantity.fromAdd.classList.remove('randomize__quantity');
         quantity.fromAdd.classList.add('randomize__quantity--disabled');
         quantity.fromAdd.disabled = true;
@@ -46,13 +45,13 @@ function handleFromAddClick() {
 function handleToRemoveClick() {
     inputTo.value = Number(inputTo.value) - 1;
 
-    if (inputTo.value == 1) {
+    if (inputTo.value == toMin) {
         quantity.toRemove.classList.remove('randomize__quantity');
         quantity.toRemove.classList.add('randomize__quantity--disabled');
         return quantity.toRemove.disabled = true;
     }
 
-    if (inputTo.value <= 9999) {
+    if (inputTo.value <= toMax - 1) {
         quantity.toAdd.classList.add('randomize__quantity');
         quantity.toAdd.classList.remove('randomize__quantity--disabled');
         quantity.toAdd.disabled = false;
@@ -62,13 +61,13 @@ function handleToRemoveClick() {
 function handleToAddClick() {
     inputTo.value = Number(inputTo.value) + 1;
 
-    if (inputTo.value >= 2) {
+    if (inputTo.value >= toMin + 1) {
         quantity.toRemove.classList.add('randomize__quantity');
         quantity.toRemove.classList.remove('randomize__quantity--disabled');
         quantity.toRemove.disabled = false;
     }
 
-    if (inputTo.value == 10000) {
+    if (inputTo.value == toMax) {
         quantity.toAdd.classList.remove('randomize__quantity');
         quantity.toAdd.classList.add('randomize__quantity--disabled');
         quantity.toAdd.disabled = true;
@@ -79,13 +78,13 @@ function handleToAddClick() {
 function handleAmountRemoveClick() {
     inputAmount.value = Number(inputAmount.value) - 1;
 
-    if (inputAmount.value == 2) {
+    if (inputAmount.value == amountMin) {
         quantity.amountRemove.classList.remove('randomize__quantity');
         quantity.amountRemove.classList.add('randomize__quantity--disabled');
         return quantity.amountRemove.disabled = true;
     }
 
-    if (inputAmount.value <= 99) {
+    if (inputAmount.value <= amountMax - 1) {
         quantity.amountAdd.classList.add('randomize__quantity');
         quantity.amountAdd.classList.remove('randomize__quantity--disabled');
         quantity.amountAdd.disabled = false;
@@ -95,13 +94,13 @@ function handleAmountRemoveClick() {
 function handleAmountAddClick() {
     inputAmount.value = Number(inputAmount.value) + 1;
 
-    if (inputAmount.value >= 3) {
+    if (inputAmount.value >= amountMin + 1) {
         quantity.amountRemove.classList.add('randomize__quantity');
         quantity.amountRemove.classList.remove('randomize__quantity--disabled');
         quantity.amountRemove.disabled = false;
     }
 
-    if (inputAmount.value == 100) {
+    if (inputAmount.value == amountMax) {
         quantity.amountAdd.classList.remove('randomize__quantity');
         quantity.amountAdd.classList.add('randomize__quantity--disabled');
         quantity.amountAdd.disabled = true;
