2789f900260a4a4698abde34fdb05159fe26725e
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-09 21:22:24 +0300

    Renamed js file for inputs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/index.scss';
 import './js/number/number';
-import './js/number/number-inputs';
+import './js/number/inputs-logic';
 import './js/navigation';
 import './js/mobile-menu';
