6c2ed189cefbd39b022624d4fb4aaa2575b587a9
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-07-12 15:34:15 +0300

    Added smooth multiple appear

--- src/js/number/amount-switcher.js
+++ src/js/number/amount-switcher.js
@@ -3,6 +3,7 @@ import { inputAmount } from "./refs";
 const amountSwitcher = document.querySelector('#amount-switcher');
 const multipleWrapper = document.querySelector('.randomize__multiple');
 const sectionTitle = document.querySelector('.section__title');
+const appearWrapper = document.querySelector('.appear-wrapper');
 
 let multipleNumbers = false;
 
@@ -12,16 +13,18 @@ function handleAmountSwitch() {
     multipleNumbers = !multipleNumbers;
 
     if (multipleNumbers === true) {
-        multipleWrapper.classList.remove('hidden')
-        sectionTitle.textContent = "Generate random numbers"
-        amountSwitcher.textContent = "Single ▲"
+        multipleWrapper.classList.remove('amount-hidden');
+        appearWrapper.classList.add('appear-wrapper--show');
+        sectionTitle.textContent = "Generate random numbers";
+        amountSwitcher.textContent = "Single ▲";
         inputAmount.value = 2;
     }
 
     if (multipleNumbers === false) {
-        multipleWrapper.classList.add('hidden');
-        sectionTitle.textContent = "Generate random number"
-        amountSwitcher.textContent = "Multiple ▼"
+        multipleWrapper.classList.add('amount-hidden');
+        appearWrapper.classList.remove('appear-wrapper--show');
+        sectionTitle.textContent = "Generate random number";
+        amountSwitcher.textContent = "Multiple ▼";
         inputAmount.value = 1;
     }
 }
