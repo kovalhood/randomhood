9d216a59a8c4a97e1d20ff528b3972d019786e4b
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-08-28 16:21:14 +0300

    Fixed sorting

--- src/js/number/sorting.js
+++ src/js/number/sorting.js
@@ -7,7 +7,6 @@ const fakeInputEl = document.querySelector('.js-fake-input');
 // All of that is created for centering text in select field on iOS Safari
 for (let i = 0; i < inputs.length; i++) {
     getSelected(i);
-
     selects[i].addEventListener("change", function() {
     getSelected(i);
     });
