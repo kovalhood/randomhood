c5c4af261278155fc3c4cb76b5867ec2d7e0c50a
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-02-19 17:54:26 +0200

    Finished working logic

--- src/js/yesno/yesno.js
+++ src/js/yesno/yesno.js
@@ -28,7 +28,7 @@ let myChart = new Chart(wheel, {
     type: 'pie',
     data: {
         // Labels (values which are to be displayed on chart)
-        labels: ['yes', 'no', 'yes', 'no', 'yes', 'no'],
+        labels: ['no', 'yes', 'no', 'yes', 'no', 'yes'],
         // Settings for dataset/pie
         datasets: [
             {
@@ -57,3 +57,51 @@ let myChart = new Chart(wheel, {
     },
 });
 
+
+// Display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+    for (let i of rotationValues) {
+        //if the angleValue is between min and max then display it
+        if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+            finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+            spinButton.disabled = false;
+            break;
+        }
+    }
+};
+
+// Spinner count
+let count = 0;
+// 100 rotations for animation and last rotation for result
+let resultValue = 101;
+
+// Start spinning
+spinButton.addEventListener('click', () => {
+    spinButton.disabled = true;
+    // Empty final value
+    finalValue.innerHTML = `<p>Good Luck!</p>`;
+    // Generate random degrees to stop at
+    let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+    // Interval for rotation animation
+    let rotationInterval = window.setInterval(() => {
+        // Set rotation for piechart
+        /*
+        Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+        */
+        myChart.options.rotation = myChart.options.rotation + resultValue;
+        // Update chart with new value
+        myChart.update();
+        // If rotation > 360 reset it back to 0
+        if (myChart.options.rotation >= 360) {
+            count += 1;
+            resultValue -= 5;
+            myChart.options.rotation = 0;
+        }
+        else if (count > 15 && myChart.options.rotation == randomDegree) {
+            valueGenerator(randomDegree);
+            clearInterval(rotationInterval);
+            count = 0;
+            resultValue = 101;
+        }
+    }, 10);
+});
