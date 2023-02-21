e581cbbeaa7e8eb9e9c7d5f477aaf97bb5a74a9b
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-02-21 19:09:37 +0200

    Added amount input to dice

--- src/js/dice/dice.js
+++ src/js/dice/dice.js
@@ -2,6 +2,99 @@ const dice1 = document.querySelector('.dice-1');
 const dice2 = document.querySelector('.dice-2');
 const rollButton = document.querySelector('.roll-button');
 
+const diceAmountMin = 1;
+const diceAmountMax = 6;
+
+const diceQuantityInput = document.querySelector('#dice-amount');
+const diceQuantityRemove = document.querySelector('#dice-remove');
+const diceQuantityAdd = document.querySelector('#dice-add');
+
+diceQuantityRemove.addEventListener('click', handleQuantityRemoveClick);
+diceQuantityAdd.addEventListener('click', handleQuantityAddClick);
+
+diceQuantityInput.addEventListener('change', handleDiceQuantityChange);
+
+diceQuantityInput.addEventListener('keydown', handleInputKeydown);
+function handleInputKeydown(event) {
+    ["e", "E", "+", "-", "."].includes(event.key) && event.preventDefault();
+}
+
+function handleQuantityRemoveClick() {
+    diceQuantityInput.value = Number(diceQuantityInput.value) - 1;
+
+    if (diceQuantityInput.value == diceAmountMin) {
+        diceRemoveButtonOff();
+    }
+
+    if (diceQuantityInput.value <= diceAmountMax - 1) {
+        diceAddButtonOn();
+    }
+}
+
+function handleQuantityAddClick() {
+    diceQuantityInput.value = Number(diceQuantityInput.value) + 1;
+
+    if (diceQuantityInput.value >= diceAmountMin + 1) {
+        diceRemoveButtonOn();
+    }
+
+    if (diceQuantityInput.value == diceAmountMax) {
+        diceAddButtonOff();
+    }
+}
+
+function handleDiceQuantityChange() {
+    if (diceQuantityInput.value <= diceAmountMin) {
+        diceQuantityInput.value = diceAmountMin;
+        diceRemoveButtonOff();
+        diceAddButtonOn();
+    }
+
+    if (diceQuantityInput.value >= diceAmountMax) {
+        diceQuantityInput.value = diceAmountMax;
+        diceRemoveButtonOn();
+        diceAddButtonOff();
+    }
+}
+
+function diceRemoveButtonOff() {
+    diceQuantityRemove.classList.remove('dice__quantity');
+    diceQuantityRemove.classList.add('dice__quantity--disabled');
+    return diceQuantityRemove.disabled = true;
+}
+
+function diceRemoveButtonOn() {
+    diceQuantityRemove.classList.add('dice__quantity');
+    diceQuantityRemove.classList.remove('dice__quantity--disabled');
+    diceQuantityRemove.disabled = false;
+}
+
+function diceAddButtonOff() {
+    diceQuantityAdd.classList.remove('dice__quantity');
+    diceQuantityAdd.classList.add('dice__quantity--disabled');
+    diceQuantityAdd.disabled = true;
+}
+
+function diceAddButtonOn() {
+    diceQuantityAdd.classList.add('dice__quantity');
+    diceQuantityAdd.classList.remove('dice__quantity--disabled');
+    diceQuantityAdd.disabled = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const randomDice = () => {
     const randomNumber = Math.floor(Math.random() * 10);
     const randomNumber1 = Math.floor(Math.random() * 10);
