54eef05e407da54f66896ac767bfc848b434e14e
Valentyn Kovalchuk <kovalhood@gmail.com>
2023-02-26 18:48:21 +0200

    Added refresh of current page after 30min

--- src/js/header/local-storage-page.js
+++ src/js/header/local-storage-page.js
@@ -7,10 +7,29 @@ import { mobileNavNumber } from "./mobile-menu";
 loadingPreviousPage();
 
 function settingCurrentPage(page) {
+    // Logic for storing time of latest activity on page
+    let currentTime = new Date();
+    let expirationTime = new Date();
+
+    // Setting 30 minutes expiration time
+    expirationTime.setTime(currentTime.getTime() + (30 * 60 * 1000));
+
     localStorage.setItem('current-page', `${page}`);
+    localStorage.setItem('page-expiration-time', expirationTime);
 }
 
 function loadingPreviousPage() {
+    // Comparing latest page load time and current page load time
+    let pageLoadTime = new Date();
+    let latestPageLoadTime = localStorage.getItem('page-expiration-time');
+
+    // If the difference is less than 30 minutes, then we will see latest page loaded
+    // If the difference is more than 30 minutes, then Number page will be loaded as default page
+    if (String(pageLoadTime) > String(latestPageLoadTime)) {
+        localStorage.setItem('current-page', 'Number');
+    }
+
+    // Working with current page display
     const previousPage = localStorage.getItem('current-page');
 
     if (previousPage === 'Number') {
